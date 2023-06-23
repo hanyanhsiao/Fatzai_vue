ee4174728fd323b59a4818f9c27b397ec5e4c461
hanyanhsiao <j0955551237@gmail.com>
2023-06-24 00:37:46 +0800

    引入 swiper套件

--- src/components/ItemCarousel.vue
+++ src/components/ItemCarousel.vue
@@ -1,219 +1,238 @@
 <script setup>
-// import { defineComponent } from "vue";
-import { Carousel, Navigation, Slide, Pagination } from "vue3-carousel";
-import "vue3-carousel/dist/carousel.css";
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Navigation, Pagination } from 'swiper'
 
-const settings = {
-    itemsToShow: 4,
-    snapAlign: "start",
-    wrapAround: true
-};
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+const modules = [Navigation, Pagination]
 
 const breakpoints = {
-    700: {
-        itemsToShow: 2,
-        snapAlign: "start",
+    0: {
+        slidesPerView: 2,
+        spaceBetween: 20,
     },
-    1024: {
-        itemsToShow: 4,
-        snapAlign: "start",
+    560: {
+        slidesPerView: 3,
+        spaceBetween: 20,
     },
-};
-
-// const componentOptions = defineComponent({
-//   name: "Breakpoints",
-//   components: {
-//     Carousel,
-//     Slide,
-//     Navigation,
-//   },
-//   setup() {
-//     return {
-//       settings,
-//       breakpoints,
-//     };
-//   },
-// });
-
-// export { componentOptions };
+    768: { slidesPerView: 4, spaceBetween: 20 },
+}
+// 下方點點
+const pagination = {
+    el: '.dots',
+    type: 'bullets',
+    bulletActiveClass: 'dotsActive',
+}
+// 左右箭頭
+const navigation = {
+    nextEl: '.arrow_next',
+    prevEl: '.arrow_pre',
+}
 </script>
 
 <template>
-    <Carousel v-bind="settings" :breakpoints="breakpoints" class="items carousel" ref="myCarousel">
-        <!-- <Slide v-for="slide in 10" :key="slide">
-                <div class="carousel__item">{{ slide }}</div>
-              </Slide> -->
-        <Slide class="it carousel__item" :key="1">
-            <router-link to="/ItemView">
-                <div class="img_block">
-                    <img src="../assets/image/items/mousse7.jpg" alt="" />
+    <div class="carousel_container">
+        <div class="arrow_pre"></div>
+        <swiper
+            :modules="modules"
+            :breakpoints="breakpoints"
+            :pagination="pagination"
+            :navigation="navigation"
+            loop
+            class="items"
+        >
+            <swiper-slide>
+                <div class="it">
+                    <router-link to="/ItemView">
+                        <div class="img_block">
+                            <img src="../assets/image/items/mousse7.jpg" alt="" />
+                        </div>
+                        <div class="text_block">
+                            <h3>Cassis Mousse</h3>
+                            <p>$180</p>
+                        </div>
+                    </router-link>
                 </div>
-                <div class="text_block">
-                    <h3>Cassis Mousse</h3>
-                    <p>$180</p>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="it">
+                    <router-link to="/ItemView">
+                        <div class="img_block">
+                            <img src="../assets/image/items/mousse8.jpg" alt="" />
+                        </div>
+                        <div class="text_block">
+                            <h3>Cassis Mousse</h3>
+                            <p>$180</p>
+                        </div>
+                    </router-link>
                 </div>
-            </router-link>
-        </Slide>
-        <Slide class="it carousel__item" :key="2">
-            <router-link to="/ItemView">
-                <div class="img_block">
-                    <img src="../assets/image/items/mousse8.jpg" alt="" />
+            </swiper-slide>
+            <swiper-slide>
+                <div class="it">
+                    <router-link to="/ItemView">
+                        <div class="img_block">
+                            <img src="../assets/image/items/mousse8.jpg" alt="" />
+                        </div>
+                        <div class="text_block">
+                            <h3>Pistache Mousse</h3>
+                            <p>$250</p>
+                        </div>
+                    </router-link>
                 </div>
-                <div class="text_block">
-                    <h3>Pistache Mousse</h3>
-                    <p>$250</p>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="it">
+                    <router-link to="/ItemView">
+                        <div class="img_block">
+                            <img src="../assets/image/items/mousse7.jpg" alt="" />
+                        </div>
+                        <div class="text_block">
+                            <h3>Cassis Mousse</h3>
+                            <p>$180</p>
+                        </div>
+                    </router-link>
                 </div>
-            </router-link>
-        </Slide>
-        <Slide class="it carousel__item" :key="3">
-            <router-link to="/ItemView">
-                <div class="img_block">
-                    <img src="../assets/image/items/mousse7.jpg" alt="" />
+            </swiper-slide>
+            <swiper-slide>
+                <div class="it">
+                    <router-link to="/ItemView">
+                        <div class="img_block">
+                            <img src="../assets/image/items/mousse5.jpg" alt="" />
+                        </div>
+                        <div class="text_block">
+                            <h3>Chocolate Yo-Yo Cake</h3>
+                            <p>$199</p>
+                        </div>
+                    </router-link>
                 </div>
-                <div class="text_block">
-                    <h3>Tiramisu Tart</h3>
-                    <p>$199</p>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="it">
+                    <router-link to="/ItemView">
+                        <div class="img_block">
+                            <img src="../assets/image/items/mousse9.jpg" alt="" />
+                        </div>
+                        <div class="text_block">
+                            <h3>Vanilla Cake Role</h3>
+                            <p>$160</p>
+                        </div>
+                    </router-link>
                 </div>
-            </router-link>
-        </Slide>
-        <Slide class="it carousel__item" :key="4">
-            <router-link to="/ItemView">
-                <div class="img_block">
-                    <img src="../assets/image/items/mousse5.jpg" alt="" />
+            </swiper-slide>
+            <swiper-slide>
+                <div class="it">
+                    <router-link to="/ItemView">
+                        <div class="img_block">
+                            <img src="../assets/image/items/mousse7.jpg" alt="" />
+                        </div>
+                        <div class="text_block">
+                            <h3>Cassis Mousse</h3>
+                            <p>$180</p>
+                        </div>
+                    </router-link>
                 </div>
-                <div class="text_block">
-                    <h3>Chocolate Yo-Yo Cake</h3>
-                    <p>$199</p>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="it">
+                    <router-link to="/ItemView">
+                        <div class="img_block">
+                            <img src="../assets/image/items/mousse7.jpg" alt="" />
+                        </div>
+                        <div class="text_block">
+                            <h3>Cassis Mousse</h3>
+                            <p>$180</p>
+                        </div>
+                    </router-link>
                 </div>
-            </router-link>
-        </Slide>
-        <Slide class="it carousel__item" :key="5">
-            <router-link to="/ItemView">
-                <div class="img_block">
-                    <img src="../assets/image/items/mousse9.jpg" alt="" />
-                </div>
-                <div class="text_block">
-                    <h3>Vanilla Cake Role</h3>
-                    <p>$160</p>
-                </div>
-            </router-link>
-        </Slide>
-        <Slide class="it carousel__item" :key="6">
-            <router-link to="/ItemView">
-                <div class="img_block">
-                    <img src="../assets/image/items/mousse9.jpg" alt="" />
-                </div>
-                <div class="text_block">
-                    <h3>Vanilla Cake Role</h3>
-                    <p>$160</p>
-                </div>
-            </router-link>
-        </Slide>
-        <Slide class="it carousel__item" :key="7">
-            <router-link to="/ItemView">
-                <div class="img_block">
-                    <img src="../assets/image/items/mousse9.jpg" alt="" />
-                </div>
-                <div class="text_block">
-                    <h3>Vanilla Cake Role</h3>
-                    <p>$160</p>
-                </div>
-            </router-link>
-        </Slide>
-        <Slide class="it carousel__item" :key="8">
-            <router-link to="/ItemView">
-                <div class="img_block">
-                    <img src="../assets/image/items/mousse9.jpg" alt="" />
-                </div>
-                <div class="text_block">
-                    <h3>Vanilla Cake Role</h3>
-                    <p>$160</p>
-                </div>
-            </router-link>
-        </Slide>
-
-        <template #addons>
-            <Navigation />
-            <Pagination />
-        </template>
-    </Carousel>
+            </swiper-slide>
+        </swiper>
+        <div class="arrow_next"></div>
+    </div>
+    <div class="dots"></div>
 </template>
 
 <style lang="scss">
-.items {
-    outline: 2px solid rgb(255, 0, 0);
-    // display: flex;
-    margin: 0 100px 20px;
+.carousel_container {
+  // outline: 2px solid rgb(255, 0, 0);
 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .items {
+    // outline: 2px solid rgb(255, 0, 0);
+    margin: 20px;
+    position: relative;
 
     .it {
-        /* border-radius: 5px; */
-        border: 1px solid #ccc;
-        // margin: 10px;
+      /* border-radius: 5px; */
+      border: 1px solid #ccc;
+      overflow: hidden;
 
+      .img_block {
+        /* outline: 2px solid black; */
+        position: relative;
         overflow: hidden;
 
-        .img_block {
-            /* outline: 2px solid black; */
-            position: relative;
-            overflow: hidden;
+        img {
+          width: 100%;
+          display: block;
+          transform: scale(1);
+          transition: all 0.5s ease;
 
-            img {
-                width: 100%;
-                display: block;
-                transform: scale(1);
-                transition: all 0.5s ease;
+          &:hover {
+            transform: scale(1.2);
+          }
+        }
+      }
 
-                &:hover {
-                    transform: scale(1.2);
-                }
-            }
+      .text_block {
+        /* outline: 2px solid rgb(255, 0, 0); */
+        line-height: 2;
+        text-align: center;
+
+        h3 {
+          color: $secondary_color;
         }
 
-        .text_block {
-            /* outline: 2px solid rgb(255, 0, 0); */
-            line-height: 2;
-            text-align: center;
-
-            h3 {
-                color: $secondary_color;
-            }
-
-            p {
-                background-color: $primary_color;
-                text-align: center;
-            }
+        p {
+          background-color: $primary_color;
+          text-align: center;
         }
+      }
     }
-}
+  }
 
-.Slide {
-    // margin-right: 0 10px;
-    /* 轮播项之间的间距，根据需要进行调整 */
-}
-
-.carousel__item {}
-
-.carousel__slide {
-    // padding: 10px;
-}
-
-.carousel__prev {
+  /* 輪播箭頭 */
+  .arrow_pre:before {
+    content: "〈";
+    font-size: 2rem;
+    line-height: 0;
+    font-weight: 500;
+    cursor: pointer;
     color: $secondary_color;
-    width: 60px;
-    left: -60px;
+  }
 
-
-}
-
-.carousel__next {
+  .arrow_next:before {
+    content: "〉";
+    font-size: 2rem;
+    line-height: 0;
+    font-weight: 500;
+    cursor: pointer;
     color: $secondary_color;
-    width: 60px;
-    right: -60px;
+  }
 }
 
-.carousel__icon {
-    width: 50px;
-    height: 50px;
+.dots {
+  text-align: center;
+}
+
+.dotsActive {
+  background-color: $secondary_color;
+  opacity: 1;
 }
 </style>
